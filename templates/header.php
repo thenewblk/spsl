aee3983a5ebb740b8e5f6a822ab29c7786f0a720
Alex Rock <hi@lxrck.com>
2015-08-06 15:28:14 -0500

    Thrusday header additions

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/spsl_logo.svg" /></a>
     <nav role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
