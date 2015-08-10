7cfabdbb48e0c87d4840e7caebb250811c148a0f
bangarang <me@alexjrock.com>
2015-08-10 04:42:52 -0500

    Story Start and header stuffs

--- story-page.php
+++ story-page.php
@@ -6,6 +6,8 @@
 
 <div class="page_header" >
   <h2 class="page_title cm">SPSL STORY</h2>
+  <?php $url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large' ); $url = $url['0']; ?>
+  <?php echo $url ?>
 </div>
 
 <div class="container">
